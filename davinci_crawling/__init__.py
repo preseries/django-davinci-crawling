f8d90e88f43012d23e63f0f7296e363f36be1c98
xalperte <javier.alperte@gmail.com>
2018-11-22 17:13:45 +0100

    Fix project version

--- davinci_crawling/__init__.py
+++ davinci_crawling/__init__.py
@@ -9,7 +9,7 @@ r"""
 """
 
 __title__ = 'Django DaVinci Crawling Framework'
-__version__ = '1.0.0'
+__version__ = '0.1.0'
 __author__ = 'Javier Alperte'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018-2019 PreSeries Tech S.L.'
