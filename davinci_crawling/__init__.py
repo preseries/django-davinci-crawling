878c45a3aadc0969b0e15eb799ce37fc6d87a954
xalperte <javier.alperte@gmail.com>
2018-11-29 18:19:42 +0100

    Changed the version from 0.1.1 to 0.1.2

--- davinci_crawling/__init__.py
+++ davinci_crawling/__init__.py
@@ -9,7 +9,7 @@ r"""
 """
 
 __title__ = 'Django DaVinci Crawling Framework'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Javier Alperte'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018-2019 PreSeries Tech S.L.'
