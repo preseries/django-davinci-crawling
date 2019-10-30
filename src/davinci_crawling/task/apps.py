7f8fb4790f38502d16b6bc5875329539bcde1260
Bruno <bruno.henriquy@gmail.com>
2019-10-29 20:47:39 -0500

    Force import of serializers on start through the Django AppConfig interface.

---
+++ src/davinci_crawling/task/apps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*
+# Copyright (c) 2019 BuildGroup Data Services Inc.
+# All rights reserved.
+
+from django.apps import AppConfig
+
+
+class DaVinciCrawlerConfig(AppConfig):
+    name = 'davinci_crawling.task'
+    verbose_name = "Django DaVinci Crawler task"
+
+    def ready(self):
+        from davinci_crawling.task.api import serializers
