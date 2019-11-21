f3ebcbc42193b3138f17e0bbb1e4cf4f0dce2244
Joao Medeiros <joaoribeiromedeiros@gmail.com>
2019-11-21 09:43:54 -0300

    Add thread test to redis throttle

--- src/davinci_crawling/throttle/tests/test_redis_throttle.py
+++ src/davinci_crawling/throttle/tests/test_redis_throttle.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2019 BuildGroup Data Services Inc.
 import logging
+import threading
 
 from caravaggio_rest_api.tests import CaravaggioBaseTest
 from davinci_crawling.throttle.redis_throttle import RedisThrottle
@@ -40,3 +41,30 @@ class TestRedisThrottle(CaravaggioBaseTest):
             else:
                 self.assertEqual(0, times_throttled)
                 quantity += 1
+
+    def test_thread_throttle(self):
+
+        def do_thread(results):
+            throttle = RedisThrottle("test", seconds=10, rate=10,
+                                     max_tokens=10)
+
+            times_throttled = 0
+            for x in range(10):
+                _times_throttled = throttle.wait_for_token("test")
+                times_throttled += _times_throttled
+
+            results.append(times_throttled)
+
+        results = []
+        t1 = threading.Thread(target=do_thread, args=(results,))
+        t2 = threading.Thread(target=do_thread, args=(results,))
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+        self.assertTrue(results)
+        for result in results:
+            self.assertTrue(result > 0)
