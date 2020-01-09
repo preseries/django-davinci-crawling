3da76ceee0f871161e3576867f00234f374f7d41
Joao Medeiros <joaoribeiromedeiros@gmail.com>
2020-01-09 12:31:28 -0300

    Change columns lists to have only the root fields

--- src/davinci_crawling/entity_diff/diff.py
+++ src/davinci_crawling/entity_diff/diff.py
@@ -22,6 +22,16 @@ def _get_deletes(translated_result):
     return _get_translated_result(translated_result, "deletes")
 
 
+def _add_field_to_list(field_name, list_to_add):
+    if "." in field_name:
+        field_name = field_name.split(".")[0] + "*"
+
+    if "[" in field_name:
+        field_name = field_name.split("[")[0] + "*"
+
+    list_to_add.add(field_name)
+
+
 def _translate(json_diff_result, translated_result, inserted_fields,
                updated_fields, deleted_fields, last_field=""):
     """
@@ -31,13 +41,6 @@ def _translate(json_diff_result, translated_result, inserted_fields,
         translated_result: the dict that will store the translated response;
         inserted_fields: the name of the fields that have been inserted;
         updated_fields: the name of the fields that have been updated;
-        deleted_fields: the name of the fields that have been deleted;
-        last_field: when we have a change on a multi-level dict we need to
-            iterate over the levels and because of that we need to know the
-            name of the last field
-
-    Returns:
-
     """
     if last_field:
         last_field += "."
@@ -50,7 +53,7 @@ def _translate(json_diff_result, translated_result, inserted_fields,
                         _get_inserts(translated_result)[last_field + field] = {
                             "new_value": value_field
                         }
-                        inserted_fields.append(last_field + field)
+                        _add_field_to_list(last_field + field, inserted_fields)
                 elif isinstance(value, list):
                     for tuple_val in value:
                         # this guy is inserting a whole object on this position
@@ -62,14 +65,14 @@ def _translate(json_diff_result, translated_result, inserted_fields,
                             _get_inserts(translated_result)[last_field] = {
                                 "new_value": tuple_val[1]
                             }
-                            inserted_fields.append(last_field)
+                            _add_field_to_list(last_field, inserted_fields)
             elif str(key) == "$delete":
                 if isinstance(value, dict):
                     for field, value_field in value.items():
                         _get_deletes(translated_result)[last_field + field] = {
                             "old_value": value_field
                         }
-                        deleted_fields.append(last_field + field)
+                        _add_field_to_list(last_field + field, deleted_fields)
                 elif isinstance(value, list):
                     for tuple_val in value:
                         # this guy is inserting a whole object on this position
@@ -81,7 +84,7 @@ def _translate(json_diff_result, translated_result, inserted_fields,
                             _get_deletes(translated_result)[last_field] = {
                                 "old_value": tuple_val[1]
                             }
-                            deleted_fields.append(last_field)
+                            _add_field_to_list(last_field, deleted_fields)
 
         elif isinstance(key, str):
             # this is when jsondiff founds an update on the value
@@ -92,7 +95,7 @@ def _translate(json_diff_result, translated_result, inserted_fields,
                     "new_value": new_value,
                     "old_value": old_value
                 }
-                updated_fields.append(last_field + key)
+                _add_field_to_list(last_field + key, updated_fields)
             # this is when we have a change in a multilevel environment
             elif isinstance(value, dict):
                 _translate(value, translated_result, inserted_fields,
@@ -110,21 +113,57 @@ def _translate(json_diff_result, translated_result, inserted_fields,
 
 
 def make_diff(previous_json, current_json):
+    """
+    Calculates the diff between two json's.
+
+    **IMPORTANT**: please be sure to sort all your list of dictionaries with a
+    specific key before saving it to db, without this guarantee this method
+    will not work 100% and probably will generate extra diffs.
+
+    Args:
+        previous_json: the previous version of the dict;
+        current_json: the current version of the dict;
+    Returns:
+        A dict containing the result of the diff on this format:
+        {
+          "all": {
+            "inserts": {
+              "column_name": {
+                "new_value": 1111111
+              }
+            },
+            "updates": {
+              "column_name": {
+                "old_value": 1,
+                "new_value": 2
+              }
+            },
+            "deletes": {
+              "column_name": {
+                "old_value": 1
+              }
+            }
+          },
+          "inserts": ["column_name"],
+          "updates": ["column_name"],
+          "deletes": ["column_name"]
+        }
+    """
     result = diff(previous_json, current_json, syntax='symmetric')
 
     translated_result = {}
-    inserted_fields = []
-    updated_fields = []
-    deleted_fields = []
+    inserted_fields = set()
+    updated_fields = set()
+    deleted_fields = set()
 
     _translate(result, translated_result, inserted_fields, updated_fields,
                deleted_fields)
 
     final_result = {
         "all": translated_result,
-        "inserts": inserted_fields,
-        "updates": updated_fields,
-        "deletes": deleted_fields
+        "inserts": list(inserted_fields),
+        "updates": list(updated_fields),
+        "deletes": list(deleted_fields)
     }
 
     return final_result
